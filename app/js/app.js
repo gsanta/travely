695911824fc18497d13fd17c8b662c7d9aa67a0a
gsanta <santa_gergely@hotmail.com>
2014-02-06 22:43:24 +0100

    added compass instead of plain sass

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,5 @@
-
-require('templates');
 require('dependencies/js/jquery/jquery-1.9.1');
 require('dependencies/js/jquery/jquery-ui-1.10.3.custom.min');
-require('dependencies/js/handlebars/handlebars-runtime');
 require('dependencies/js/ember/ember');
 require('dependencies/js/ember/ember-data');
 require('dependencies/js/bootstrap/bootstrap.min');
@@ -11,3 +8,24 @@ require('dependencies/js/compiled/templates');
 window.App = Ember.Application.create();
 
 require('app/js/routes/router');
+
+$(function() {
+	var client = new Dropbox.Client( { key: '8z2mismtbwrgoep' } );
+
+	// Try to finish OAuth authorization.
+	client.authenticate({interactive: false}, function (error) {
+		console.log("authentication")
+	    if (error) {
+	        alert('Authentication error: ' + error);
+	    }
+	});
+
+	if (client.isAuthenticated()) {
+		console.log("autentikalva vagyok");
+	    // Client is authenticated. Display UI.
+	}
+
+	$("#testButton").click(function() {
+		client.authenticate();
+	});
+});
--- app/js/routes/router.js
+++ app/js/routes/router.js
@@ -1,11 +1,11 @@
 App.Router.map(function () {
-  this.route("map", { path: "/" });
-  this.route("photos");
-  this.route("setup");
-  this.resource("notes", function() {
-  	this.resource("note", { path: "/:note_id" }, function() {
-  		this.route( "edit" );
-  	} );
-  	this.route( "create" );
-  } )
+    this.route("map", { path: "/" });
+    this.route("photos");
+    this.route("setup");
+    this.resource("notes", function() {
+        this.resource("note", { path: "/:note_id" }, function() {
+            this.route( "edit" );
+        } );
+        this.route( "create" );
+    } );
 });
